c1e124e4d97cf4b24fbc66db922afaefbc65bbd8
Leo Galassi <leo.galassi1@etu.univ-lorraine.fr>
2018-10-18 17:02:23 +0200

    Ajout de la nouvelle carte question en base de donnée.

--- src/controllers/PlayerController.php
+++ src/controllers/PlayerController.php
@@ -3,6 +3,7 @@
 namespace trivial\controllers;
 
 use trivial\views\PlayerView;
+use trivial\models as m;
 
 class PlayerController{
 
@@ -22,8 +23,28 @@ class PlayerController{
 		$question = $_POST['question'] ;
         $reponse = $_POST['reponse'] ;
         
-       
-      
+        //Je récupère l'id du thème pour pouvoir la question
+        $themeId = m\Theme::where('nomTheme','=',$theme);
+        $themeId = $themeId->first();
+        $id = $themeId->idTheme;
+        self::createQuestions($id,$question,$reponse);
+
+        global $app ;
+
+        $url =  $app->getContainer()->get('router')->pathFor('MyAccount');
+  
+        header("Location: $url");
+        exit();
 
     }
+
+   public static function createQuestions($id,$question,$reponse){
+       $carte = new m\Carte();
+
+       $carte->idTheme = $id;
+       $carte->question = $question;
+       $carte->reponse = $reponse;
+
+       $carte->save();
+   }
 }
