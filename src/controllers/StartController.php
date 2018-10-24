0f46310d4b5008e533167c45caf2365b53ecad80
Leo Galassi <leo.galassi1@etu.univ-lorraine.fr>
2018-10-24 08:55:57 +0200

    Le joueur qui crée son salon se voit attribuer le même idSalon que le salon.

--- src/controllers/StartController.php
+++ src/controllers/StartController.php
@@ -5,6 +5,7 @@ namespace trivial\controllers;
 use trivial\views\StartView;
 use trivial\views\SaloonView;
 use trivial\models as m;
+use trivial\controllers\JoinController;
 
 
 class StartController {
@@ -28,6 +29,7 @@ class StartController {
 		$mode = $_POST['mode'] ;
 		$saloon = $_POST['salon'] ;
 		
+		
 		//mode devient un integer pour respecter la structure de la BDD
 		if($mode == "Privé"){
 			$mode = 1;
@@ -37,24 +39,27 @@ class StartController {
 		}
 		
 		self::createSaloon($mode,$saloon);
-		setcookie('salon', $saloon, time() + 365*24*3600, null, null, false, true);
-		global $app ;
-
-        $url =  $app->getContainer()->get('router')->pathFor('Saloon',[
-			'name' => $saloon
-		]);
-  
-        header("Location: $url");
-        exit();
+		
 	}
 
 	public static function createSaloon($mode,$saloon){
+		//Création du salon
 		$salon = new m\Salon();
 
 		$salon->nomSalon = $saloon;
 		$salon->visible = $mode;
 		
 		$salon->save();
+		// L'idSalon de la table Salon se synchronise avec l'idSalon de la table Joueur
+		$salon= m\Salon::where('nomSalon','=',$saloon);
+		$salon= $salon->first();
+		$idSalon = $salon->idSalon;
+		$idJoueur = $_SESSION['idJoueur'];
+		//La synchronisation se fait à travers la méthode joinSaloon.
+		//Ainsi un joueur qui crée un salon n'est ni plus ni moins qu'un
+		//joueur qui rejoint un salon, sauf que dans ce cas, le salon 
+		//vient d'être crée.
+		JoinController::joinSaloon($saloon,$idSalon,$idJoueur);
 	}
 
 }
