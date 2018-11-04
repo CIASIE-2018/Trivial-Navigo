9337c6b601d802635cea41735ebf46b7ac5dcc62
Rimet <quentin.rimet2@etu.univ-lorraine.fr>
2018-11-04 18:50:02 +0100

    Revert "Merge pull request #35 from CIASIE-2018/salonPrive"
    This reverts commit 6d62f0d8f12a16f56fe3fbcd87c8af040583ec8c, reversing
    changes made to 6480bada9c7f806da3f7753ba0fa3f0a60151f71.

--- src/controllers/SaloonController.php
+++ src/controllers/SaloonController.php
@@ -102,7 +102,6 @@ class SaloonController {
 		$saloon = m\Salon::where('nomSalon', '=', $nameSaloon);
 		$salonF = $saloon->first();
 		$idSaloon = $salonF->idSalon;
-		$visible = $salonF->visible;
 		$player = m\Joueur::where('idSalon', '=', $idSaloon)->get();
 		$listPlayers = array();
 		foreach($player as $namePlayer) {
@@ -118,7 +117,6 @@ class SaloonController {
 			'pseudo' => $pseudo,
 			'nameSaloon' => $nameSaloon,
 			'listeJoueur' => $listPlayers,
-			'visible' => $visible,
 		]);
 	}
 
